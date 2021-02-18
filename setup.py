09689785b33bb8d56d8e6bd2008f1fb65aad13e4
thomfoster <fosterthom16@gmail.com>
2021-02-18 16:02:03 +0000

    switch url to https

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='geneidatamodel',
     version='0.0.1',
     description='Private Genei Package dealing with internal AI data model for processing resources.',
-    url='git@github.com:Genei-Ltd/genei-data-model.git',
+    url='https://github.com/Genei-Ltd/genei-data-model.git',
     author='Thomas Foster',
     author_email='foster@genei.io',
     license='unlicense',
