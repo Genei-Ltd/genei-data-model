24a1cf4948aa3dd622e9e3ea5f6156f681082647
thomfoster <fosterthom16@gmail.com>
2021-02-18 15:59:12 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='geneidatamodel',
+    version='0.0.1',
+    description='Private Genei Package dealing with internal AI data model for processing resources.',
+    url='git@github.com:Genei-Ltd/genei-data-model.git',
+    author='Thomas Foster',
+    author_email='foster@genei.io',
+    license='unlicense',
+    packages=['geneidatamodel'],
+    zip_safe=False
+)
